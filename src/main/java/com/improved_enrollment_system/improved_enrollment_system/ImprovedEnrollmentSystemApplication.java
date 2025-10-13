6c64dfa1260a66b991cda9b7eb2ca7c94ba09e17
ishaansingh <ishaansingh418@gmail.com>
2025-10-13 17:41:58 -0500

    Initial commit — Spring Boot project setup

---
+++ src/main/java/com/improved_enrollment_system/improved_enrollment_system/ImprovedEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.improved_enrollment_system.improved_enrollment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovedEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovedEnrollmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/improved_enrollment_system/improved_enrollment_system/ImprovedEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improved_enrollment_system.improved_enrollment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovedEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
